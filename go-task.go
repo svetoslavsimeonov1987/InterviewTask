5293bdfd361e2243beff7ab6a76f3c7ed57a2150
svetoslavsimeonov1987 <32739597+svetoslavsimeonov1987@users.noreply.github.com>
2024-08-30 09:59:24 +0300

    Create go-task.go

---
+++ go-task.go
@@ -0,0 +1,21 @@
+// Question: What will happen when you run this GO code?
+
+package main
+
+import (
+	"fmt"
+)
+
+const number = 5
+
+func main() {
+	var number int64
+	number = 10 + number
+	addFive(number)
+	fmt.Print(number, "\n")
+}
+
+func addFive(num int64) int64 {
+	newNumber := num + 5
+	return newNumber
+}
