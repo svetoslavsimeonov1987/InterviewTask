95a038d0f4fdc2356f8277de43a7da92be9af664
svetoslavsimeonov1987 <32739597+svetoslavsimeonov1987@users.noreply.github.com>
2024-12-09 11:30:27 +0200

    Update go-task.go

--- go-task.go
+++ go-task.go
@@ -7,11 +7,6 @@ import (
 
 func main() {
 	NUMBER := 2
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println("An error occurred.")
-		}
-	}()
 	ARR := []int{1, 2, 3, 4, 5}
 	NUMBER = 2 + int(math.Pow(2, 3)) + ARR[len(ARR)-1]
 	addFive(addFive(NUMBER))
