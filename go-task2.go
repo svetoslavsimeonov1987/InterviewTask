48fd3291d4780efd3e5c7eedf10afd6faa13f767
svetoslavsimeonov1987 <32739597+svetoslavsimeonov1987@users.noreply.github.com>
2025-02-12 16:54:34 +0200

    Create go-task2.go

---
+++ go-task2.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	NUMBER := 2
+	ARR := []int{1, 2, 3, 4, 5}
+	addFive(addFive(NUMBER))
+	NUMBER = 2 + int(math.Pow(2, 3)) + ARR[len(ARR)]
+	fmt.Println(NUMBER)
+}
+
+func addFive(num int) int {
+	newNumber := num + 5
+	return newNumber
+}
